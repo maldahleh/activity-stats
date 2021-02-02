85d51500b797863dc82dc054566073ab7be2bfa1
Mohammed Al-Dahleh <maldahleh@gmail.com>
2021-02-02 16:43:40 -0500

    Initial Commit

---
+++ ActivityStats/ActivityStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityStatsApp.swift
+//  ActivityStats
+//
+//  Created by Mohammed Al-Dahleh on 2021-02-02.
+//
+
+import SwiftUI
+
+@main
+struct ActivityStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

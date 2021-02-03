38aab3ff62db759c96acfcb1cc66aced69ae7595
Mohammed Al-Dahleh <maldahleh@gmail.com>
2021-02-03 15:29:16 -0500

    Adds boilerplate implementation for DataRow

---
+++ ActivityStats/Views/Data/DataRow.swift
@@ -0,0 +1,20 @@
+//
+//  DataRow.swift
+//  ActivityStats
+//
+//  Created by Mohammed Al-Dahleh on 2021-02-03.
+//
+
+import SwiftUI
+
+struct DataRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DataRow_Previews: PreviewProvider {
+    static var previews: some View {
+        DataRow()
+    }
+}
